17ec11e816256e65b487d0327cf96b871ac206b4
jraduege <git@lvec.org>
2022-04-30 22:01:45 -0700

    Add javascript for adding books to array

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let myLibrary = [];
+
+function Book(title,author,pages,hasRead) {
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.hasRead=hasRead;
+}
+
 const collapseButton = document.querySelector(".collapse")
 const expandButton = document.querySelector(".newbook");
 const bookAdd = document.querySelector(".book-add");
@@ -8,12 +17,19 @@ expandButton.addEventListener("click",moveSidebar);
 
 function moveSidebar() {
     let barWidth = getComputedStyle(sidebar).width;
-    console.log(barWidth);
-
     let newWidth = (barWidth==="120px") ? "300px" : "120px";
-    console.log(newWidth);
+
     collapseButton.classList.toggle("hidden");
     expandButton.classList.toggle("hidden");
     bookAdd.classList.toggle("hidden");
     sidebar.style.width=newWidth;
+}
+
+function addBook() {
+    title=document.getElementById("book-title").value;
+    author=document.getElementById("book-author").value;
+    pages=document.getElementById("book-pages").value;
+    hasRead= (document.getElementById("book-read").value==="on") ? true : false;
+    newBook = new Book(title,author,pages,hasRead);
+    myLibrary.push(newBook);
 }
