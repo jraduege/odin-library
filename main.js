9733b00ad10d79b7b8cd9d88878eb461a3a7f5a4
jraduege <git@lvec.org>
2022-05-01 10:14:52 -0700

    Add formatted but non-working buttons to cards

--- main.js
+++ main.js
@@ -26,13 +26,21 @@ function moveSidebar() {
 }
 
 function addBook() {
-    title=document.getElementById("book-title").value;
-    author=document.getElementById("book-author").value;
-    pages=document.getElementById("book-pages").value;
-    hasRead= (document.getElementById("book-read").value==="on") ? true : false;
-    newBook = new Book(title,author,pages,hasRead);
+    let title=document.getElementById("book-title").value;
+    let author=document.getElementById("book-author").value;
+    let pages=document.getElementById("book-pages").value;
+    let hasRead = document.getElementById("book-read").checked;
+    let newBook = new Book(title,author,pages,hasRead);
     myLibrary.push(newBook);
     makeCatalog();
+}
+
+function makeButtons(hasRead,index) {
+    let div = document.createElement('div');
+    let readButtonText = (hasRead) ? "Mark unread" : "Mark read";
+    div.classList.add("card-button-bar");
+    div.innerHTML=`<button class="toggle-read" data-index=${index}>${readButtonText}</button><button class="remove-button" data-index=${index}>Remove Book</button>`;
+    return div;
 }
 
 function makeCard(Book,index) {
@@ -40,6 +48,7 @@ function makeCard(Book,index) {
     div.classList.add("card");
     div.innerHTML=`<p class="title">${Book.title}</p><p>Author: ${Book.author}</p><p>Pages: ${Book.pages}</p>`
     div.setAttribute("data-index",index);
+    div.appendChild(makeButtons(Book.hasRead,index));
     return div;
 }
 
